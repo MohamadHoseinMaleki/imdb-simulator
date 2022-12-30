abb1c317f719b86b6c640934d143f8508ea6562d
mohamad <mhmm69923@gmail.com>
2022-12-30 18:29:14 +0330

    complete UserRepo.java

---
+++ src/main/java/com/example/demo/repo/Balancer/UserRepo.java
@@ -0,0 +1,21 @@
+/*
+ * *
+ * @version 1
+ * @moh-m82
+ * /
+ *
+ */
+
+/*
+ * *
+ * @version 1
+ * @moh-m82
+ * /
+ *
+ */
+
+package com.example.demo.repo.Balancer;
+
+public interface UserRepo {
+
+}
