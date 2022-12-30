b5d6eb13d3a4f91f9818147b3f852c60d63ac420
mohamad <mhmm69923@gmail.com>
2022-12-30 14:26:29 +0330

    creat  principalsTitleRepository.java in own package

--- src/main/java/com/example/demo/Repository/principalsTitleRepository.java
+++ src/main/java/com/example/demo/Repository/principalsTitleRepository.java
@@ -8,5 +8,14 @@
 
 package com.example.demo.Repository;
 
-public interface principalsTitleRepository {
+import com.example.demo.moodel.MoviesOrBasics;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface principalsTitleRepository extends JpaRepository <principalsTitleRepository, Integer> {
+
+    Optional<principalsTitleRepository> findByIdTConst(String st);
+    Optional<principalsTitleRepository> findByCategory(String st);
+    Optional<principalsTitleRepository> findByJob(String st);
 }
