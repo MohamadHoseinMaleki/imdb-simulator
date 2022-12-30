d70eeed7cec4067f3d7f86a80ddcae6b6030cc93
mohamad <mhmm69923@gmail.com>
2022-12-30 14:26:06 +0330

    creat  Actor Repository in own package

--- src/main/java/com/example/demo/Repository/ActorRepository.java
+++ src/main/java/com/example/demo/Repository/ActorRepository.java
@@ -8,6 +8,15 @@
 
 package com.example.demo.Repository;
 
-public interface ActorRepository {
+import com.example.demo.moodel.ActorOrBasicNames;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface ActorRepository extends JpaRepository <ActorOrBasicNames, Long>{
+
+    Optional<ActorOrBasicNames> findByIdnCost( int intiger );
+    Optional<ActorOrBasicNames> findByprimaryName( String st );
+    Optional<ActorOrBasicNames> findByKnownForTitles( int intigger);
 
 }
