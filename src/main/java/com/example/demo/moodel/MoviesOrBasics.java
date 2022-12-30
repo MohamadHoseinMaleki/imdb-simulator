8390151cb61d03cfa7a4d44e03463f25732056a8
mohamad <mhmm69923@gmail.com>
2022-12-30 13:49:28 +0330

    Class of Movie Service for project

---
+++ src/main/java/com/example/demo/moodel/MoviesOrBasics.java
@@ -0,0 +1,91 @@
+/*
+ * *
+ * @version 1
+ * @moh-m82
+ * /
+ *
+ */
+
+/*
+ * *
+ * @version 1
+ * @moh-m82
+ * /
+ *
+ */
+
+/*
+ * *
+ * @version 1
+ * @moh-m82
+ * /
+ *
+ */
+
+package com.example.demo.moodel;/*
+ * *
+ * @version 1
+ * @moh-m82
+ * /
+ *
+ */
+
+import java.util.Comparator;
+import java.util.Optional;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Getter
+@Setter
+@Entity
+@RequiredArgsConstructor
+
+public class MoviesOrBasics implements Comparable {
+    //idTConst	titleType	primaryTitle	originalTitle	isAdult	startYear	endYear	runtimeMinutes	genres
+    @Id
+    @GeneratedValue
+
+    private int idTConst;
+    private String titleType;
+    private String primaryTitle;
+    private int isAdult;
+    private int startYear;
+    private int endYear;
+    private int runtimeMinutes;
+    private String genres;
+
+
+    public Comparator<? super MoviesOrBasics> setEndYear(int endYear) {
+        this.endYear = endYear;
+        return null;
+    }
+
+    public void setRuntimeMinutes(int runtimeMinutes) {
+        this.runtimeMinutes = runtimeMinutes;
+    }
+
+    @Override
+    public int compareTo(Object o) {
+        MoviesOrBasics music = (MoviesOrBasics) o;
+
+        if (endYear > music.endYear) {
+            return 1;
+        } else {
+            if (endYear < music.endYear) {
+                return -1;
+            } else {
+                return 0;
+            }
+        }
+    }
+
+
+
+}
+
---
+++ src/main/java/com/example/demo/service/Movei/service/MovieService.java
@@ -0,0 +1,12 @@
+/*
+ * *
+ * @version 1
+ * @moh-m82
+ * /
+ *
+ */
+
+package com.example.demo.service.Movei.service;
+
+public class MovieService {
+}
