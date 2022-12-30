e29bc624565b04c4455d819a6a7cb45c8c26d75f
mohamad <mhmm69923@gmail.com>
2022-12-30 13:20:54 +0330

    Creat long in Request in package request

---
+++ src/main/java/com/example/demo/dto/request/LoginRequest.java
@@ -0,0 +1,44 @@
+/*
+ * *
+ * @version 1
+ * @moh-m82
+ * /
+ *
+ */
+
+/*
+ * *
+ * @version 1
+ * @moh-m82
+ * /
+ *
+ */
+
+/*
+ * *
+ * @version 1
+ * @moh-m82
+ * /
+ *
+ */
+
+/*
+ * *
+ * @version 1
+ * @moh-m82
+ * /
+ *
+ */
+
+package com.example.demo.dto.request;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+
+public class LoginRequest {
+    private String UserName;
+    private String Password;
+}
---
+++ src/main/java/com/example/demo/moodel/ActorOrBasicNames.java
@@ -0,0 +1,72 @@
+/*
+ * *
+ * @version 1
+ * @moh-m82
+ * /
+ *
+ */
+
+/*
+ * *
+ * @version 1
+ * @moh-m82
+ * /
+ *
+ */
+
+/*
+ * *
+ * @version 1
+ * @moh-m82
+ * /
+ *
+ */
+
+package com.example.demo.moodel;/*
+ * *
+ * @version 1
+ * @moh-m82
+ * /
+ *
+ */
+
+/*
+ * *
+ * @version 1
+ * @moh-m82
+ * /
+ *
+ */
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@Entity
+@Setter
+@Getter
+@RequiredArgsConstructor
+
+public class ActorOrBasicNames {
+    //nConst	primaryName	birthYear	deathYear	primaryProfession	knownForTitles
+    @Id
+    @GeneratedValue
+
+    private int idnCost;
+    private String primaryName;
+    private int birthYear;
+    private int deathYear;
+
+
+    @ManyToOne
+    @JoinColumn(name = "movie_id_t_const")
+    private MoviesOrBasics movie;
+    @ManyToMany(mappedBy = "movie")
+
+    private String primaryProfession;
+    // relation of project
+    private int knownForTitles;
+
+}
