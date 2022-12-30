22a12e7d012b3816fe6c81ed8924b0e30e8f8579
mohamad <mhmm69923@gmail.com>
2022-12-30 13:11:45 +0330

    creat model of project principals class

---
+++ src/main/java/com/example/demo/moodel/principalsTitle.java
@@ -0,0 +1,68 @@
+/*
+ * *
+ * @version 1
+ * @moh-m82
+ * /
+ *
+ */
+
+/*
+ * *
+ * @version 1
+ * @moh-m82
+ * /
+ *
+ */
+
+/*
+ * *
+ * @version 1
+ * @moh-m82
+ * /
+ *
+ */
+
+package com.example.demo.moodel;/*
+ * *
+ * @version 1
+ * @moh-m82
+ * /
+ *
+ */
+
+/*
+ * *
+ * @version 1
+ * @moh-m82
+ * /
+ *
+ */
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
+@Getter
+@Setter
+@RequiredArgsConstructor
+
+
+public class principalsTitle {
+    //idTConst	ordering	nConst	category	job	characters
+    @Id
+    @GeneratedValue
+
+    private String idTConst;
+    private int ordering;
+    private String nConst;
+    private String category;
+    private String job;
+    private String characters;
+
+}
+
