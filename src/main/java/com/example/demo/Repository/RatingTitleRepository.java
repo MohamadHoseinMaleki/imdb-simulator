3f03134a572ae58ecf82a1d833201ad691576db5
mohamad <mhmm69923@gmail.com>
2022-12-30 14:26:44 +0330

    creat  RatingTitleRepository.java in own package

--- src/main/java/com/example/demo/Repository/RatingTitleRepository.java
+++ src/main/java/com/example/demo/Repository/RatingTitleRepository.java
@@ -8,5 +8,13 @@
 
 package com.example.demo.Repository;
 
+import com.example.demo.moodel.RatingTitle;
+
+import java.util.Date;
+import java.util.Optional;
+
 public interface RatingTitleRepository {
+    Optional<RatingTitle> findByIdTConst(String st);
+    Optional<RatingTitle> findByAverageRating(Double doubl);
+    Optional<RatingTitle> findByNumVotes( int intger);
 }
