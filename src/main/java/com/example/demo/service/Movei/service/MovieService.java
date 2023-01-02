06c30efb3475a4bc3e9e24e05364ac8d27a1e0ab
mohamad <mhmm69923@gmail.com>
2023-01-02 19:33:49 +0330

    update some changes

--- src/main/java/com/example/demo/service/Movei/service/MovieService.java
+++ src/main/java/com/example/demo/service/Movei/service/MovieService.java
@@ -8,5 +8,26 @@
 
 package com.example.demo.service.Movei.service;
 
+import com.example.demo.Repository.MovieRepository;
+import com.example.demo.moodel.MoviesOrBasics;
+import org.springframework.stereotype.Service;
+
+import java.util.Collections;
+import java.util.List;
+@Service
 public class MovieService {
+
+    MovieRepository movieRepository;
+
+    public List<MoviesOrBasics> get200Top(String movie) {
+        return movieRepository .findAll(movie);
+    }
+
+//
+//    public List<MoviesOrBasics> getMovie(String movieId) {
+//        MoviesOrBasics  moviesOrBasics =movieRepository.findByMovieNum(movieId).orElse(null);
+//        assert moviesOrBasics != null;
+//        List<MoviesOrBasics> moviesOrBasics1 = (List<MoviesOrBasics>) Collections.singletonList(moviesOrBasics.getMovies());
+//        return moviesOrBasics1;
+//    }
 }
