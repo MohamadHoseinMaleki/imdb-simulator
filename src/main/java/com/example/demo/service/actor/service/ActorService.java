e3075195c8f248d762bdd816ccea6fe1ceee686f
mohamad <mhmm69923@gmail.com>
2023-01-02 19:24:08 +0330

    update some changes

--- src/main/java/com/example/demo/service/actor/service/ActorService.java
+++ src/main/java/com/example/demo/service/actor/service/ActorService.java
@@ -6,15 +6,10 @@
  *
  */
 
-/*
- * *
- * @version 1
- * @moh-m82
- * /
- *
- */
-
 package com.example.demo.service.actor.service;
 
+import org.springframework.stereotype.Service;
+
+@Service
 public class ActorService {
 }
