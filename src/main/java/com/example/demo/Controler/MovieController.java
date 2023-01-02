865e56179b3df6b06acf94952b1e31a125c5ea01
mohamad <mhmm69923@gmail.com>
2023-01-02 19:33:03 +0330

    update some changes in controller

--- src/main/java/com/example/demo/Controler/MovieController.java
+++ src/main/java/com/example/demo/Controler/MovieController.java
@@ -8,52 +8,56 @@
 
 package com.example.demo.Controler;
 
-import com.example.demo.Repository.movieRepository;
+import com.example.demo.Repository.MovieRepository;
 import com.example.demo.exception.Exceptions;
 import com.example.demo.moodel.MoviesOrBasics;
+import com.example.demo.service.Movei.service.MovieService;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 
 @RestController
 public class MovieController {
-    movieRepository movieRepository;
+    MovieRepository movieRepository;
+    MovieService movieService;
 
-
-//    @GetMapping("/movie/getTop200")
-//           public List<MoviesOrBasics> get200(@RequestBody string movie){
-//     List200Top =  movicervice.get200Top(movie);
-//             return List200Top;
-//    }
+    @GetMapping("/movie/getTop200")
+    public List<MoviesOrBasics> get200(@RequestBody String movie) {
+        List<MoviesOrBasics> List200Top = movieService.get200Top(movie);
+        return List200Top;
+    }
 
 
     @GetMapping("getMovieNum")
-    public MoviesOrBasics moviesOrBasics (@RequestParam String movieNum) throws Exceptions {
+    public MoviesOrBasics moviesOrBasics(@RequestParam String movieNum) throws Exceptions {
         return movieRepository.findByMovieNum(movieNum).orElse(null);
     }
 
 
     @GetMapping("getId")
-    public MoviesOrBasics findById (@RequestParam long movieId) throws Exceptions {
+    public MoviesOrBasics findById(@RequestParam long movieId) throws Exceptions {
         return movieRepository.findById(movieId).orElse(null);
     }
 
 
     @GetMapping("getIsAdult")
-    public MoviesOrBasics getIsAdult (@RequestParam int i) throws Exceptions {
+    public MoviesOrBasics getIsAdult(@RequestParam int i) throws Exceptions {
         return movieRepository.findIsAdult(i).orElse(null);
     }
 
     @GetMapping("getStartYear")
-    public MoviesOrBasics getStartYear (@RequestParam int startyear) throws Exceptions {
+    public MoviesOrBasics getStartYear(@RequestParam int startyear) throws Exceptions {
         return movieRepository.findstartYear(startyear).orElse(null);
     }
 
     @GetMapping("getEndYear")
-    public MoviesOrBasics getEndYear (@RequestParam int endYear) throws Exceptions {
+    public MoviesOrBasics getEndYear(@RequestParam int endYear) throws Exceptions {
         return movieRepository.findendYear(endYear).orElse(null);
     }
 
-   // MovieService moveiservice = new MovieService;
+    // MovieService moveiservice = new MovieService;
 }
