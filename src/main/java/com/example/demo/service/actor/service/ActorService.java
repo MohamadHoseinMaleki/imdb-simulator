b3571faf62b38a77702c4d9d3434aff9307feb31
mohamad <mhmm69923@gmail.com>
2023-01-02 20:40:35 +0330

    edited some cheange

--- src/main/java/com/example/demo/service/actor/service/ActorService.java
+++ src/main/java/com/example/demo/service/actor/service/ActorService.java
@@ -8,8 +8,29 @@
 
 package com.example.demo.service.actor.service;
 
+import com.example.demo.Repository.ActorRepository;
+import com.example.demo.Repository.MovieRepository;
+import com.example.demo.dto.ActorCreationResponseDTO;
+import com.example.demo.moodel.ActorOrBasicNames;
+import com.example.demo.moodel.MoviesOrBasics;
+import com.example.demo.utill.DtoToEntityConverter;
+import com.example.demo.utill.EntityToDTOConverter;
+import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Service
+@AllArgsConstructor
 public class ActorService {
+    private final ActorRepository actorRepo;
+    private final MovieRepository movieRepo;
+
+    @PostMapping("/actor")
+    public ActorCreationResponseDTO addStudent(ActorCreationResponseDTO requestDTO) {
+        ActorOrBasicNames actor  = DtoToEntityConverter.convertToActor(requestDTO);
+        MoviesOrBasics movie = movieRepo.findById(Long.valueOf(requestDTO.getActorId())).orElseThrow();
+        actor .setMovie(movie);
+        return EntityToDTOConverter.convertToDTO(actorRepo.save(actor));
+    }
+
 }
