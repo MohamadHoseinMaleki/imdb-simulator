adca0b6c65583aac59c2e1534e54cb318430044a
mohamad <mhmm69923@gmail.com>
2023-01-03 11:35:22 +0330

    creat the DtoToEntityConverter.java

--- src/main/java/com/example/demo/utill/DtoToEntityConverter.java
+++ src/main/java/com/example/demo/utill/DtoToEntityConverter.java
@@ -8,19 +8,22 @@
 
 package com.example.demo.utill;
 
-import com.example.demo.Repository.ActorRepository;
-import com.example.demo.Repository.MovieRepository;
 import com.example.demo.dto.ActorCreationResponseDTO;
 import com.example.demo.moodel.ActorOrBasicNames;
-import lombok.AllArgsConstructor;
-import org.springframework.stereotype.Service;
-import org.springframework.web.bind.annotation.PostMapping;
-
-import java.util.List;
+import com.example.demo.moodel.PrincipalsTitle;
+import com.example.demo.moodel.RattingTitle;
 
 
 public class DtoToEntityConverter {
     public static ActorOrBasicNames convertToActor(ActorCreationResponseDTO requestDTO) {
         return ActorOrBasicNames.builder().primaryName(requestDTO.getName()).primaryName(requestDTO.getName()).build();
+    }
+
+    public static PrincipalsTitle convertToMovie(ActorCreationResponseDTO requestDTO) {
+        return PrincipalsTitle.builder().nConst(requestDTO.getName()).idTConst(requestDTO.getName()).build();
+    }
+
+    public static RattingTitle convertRatingToMovie(ActorCreationResponseDTO requestDTO) {
+        return RattingTitle.builder().idTConst(requestDTO.getName()).idTConst(requestDTO.getName()).build();
     }
 }
