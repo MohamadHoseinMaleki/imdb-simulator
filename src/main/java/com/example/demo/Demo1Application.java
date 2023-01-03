aa4ef1d9b4418935737361017ea5cc86110230ed
mohamad <mhmm69923@gmail.com>
2023-01-03 12:50:51 +0330

    changes some code in class

--- src/main/java/com/example/demo/Demo1Application.java
+++ src/main/java/com/example/demo/Demo1Application.java
@@ -1,7 +1,12 @@
 package com.example.demo;
 
+import com.example.demo.enumeration.Status;
+import com.example.demo.model.Balancer.Server;
+import com.example.demo.repo.Balancer.ServerRepo;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class Demo1Application {
@@ -10,11 +15,15 @@ public class Demo1Application {
         SpringApplication.run(Demo1Application.class, args);
     }
 
-//    CommandLineRunner run (ServerRepo serverRepo){
-//        return args -> {
-//            serverRepo.save(new Server(null,"192.168.43.1","Windows 11", "16 GB", "Personal PC",
-//                    "http://localhost:8080/server/image/server1.png", Status.SERVER_UP));
-//        };
-//}
+    @Bean
+    CommandLineRunner run (ServerRepo serverRepo){
+        return args -> {
+            serverRepo.save(new Server(null,"192.168.43.1","Windows 11", "Personal PC", "http://localhost:8080/server/image/server1.png", Status.SERVER_UP));
+            serverRepo.save(new Server(null,"192.168.43.1","Windows 11", "Personal PC", "http://localhost:8080/server/image/server1.png", Status.SERVER_UP));
+            serverRepo.save(new Server(null,"192.168.43.1","Windows 11", "Personal PC", "http://localhost:8080/server/image/server1.png", Status.SERVER_UP));
+            serverRepo.save(new Server(null,"192.168.43.1","Windows 11", "Personal PC", "http://localhost:8080/server/image/server1.png", Status.SERVER_UP));
+
+        };
+}
 
 }
