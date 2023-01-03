f61b24191e18745c540c077e7e7066129d850713
mohamad <mhmm69923@gmail.com>
2023-01-03 11:36:38 +0330

    changes some code in class

--- src/main/java/com/example/demo/service/principals/principalsService.java
+++ src/main/java/com/example/demo/service/principals/principalsService.java
@@ -8,8 +8,29 @@
 
 package com.example.demo.service.principals;
 
+import com.example.demo.Repository.MovieRepository;
+import com.example.demo.Repository.PrincipalsTitleRepository;
+import com.example.demo.dto.ActorCreationResponseDTO;
+import com.example.demo.moodel.MoviesOrBasics;
+import com.example.demo.moodel.PrincipalsTitle;
+import com.example.demo.utill.DtoToEntityConverter;
+import com.example.demo.utill.EntityToDTOConverter;
+import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Service
+@AllArgsConstructor
 public class principalsService {
+    final PrincipalsTitleRepository principalsRepo;
+    final MovieRepository movieRepo;
+
+    @PostMapping("/principal")
+    public ActorCreationResponseDTO addPrinc(ActorCreationResponseDTO requestDTO) {
+        PrincipalsTitle principal = DtoToEntityConverter.convertToMovie(requestDTO);
+        MoviesOrBasics movie = movieRepo.findById(Long.valueOf(requestDTO.getActorId())).orElseThrow();
+        movie.setMovie(movie);
+        return EntityToDTOConverter.convertPrincipalToDTO((principal));
+    }
+
 }
