24bbb7db642641205331656334332b89315aef80
mohamad <mhmm69923@gmail.com>
2023-01-03 11:35:39 +0330

    creat the EntityToDTOConverter.java

--- src/main/java/com/example/demo/utill/EntityToDTOConverter.java
+++ src/main/java/com/example/demo/utill/EntityToDTOConverter.java
@@ -10,9 +10,20 @@ package com.example.demo.utill;
 
 import com.example.demo.dto.ActorCreationResponseDTO;
 import com.example.demo.moodel.ActorOrBasicNames;
+import com.example.demo.moodel.PrincipalsTitle;
+import com.example.demo.moodel.RattingTitle;
 
 public class EntityToDTOConverter {
     public static ActorCreationResponseDTO convertToDTO(ActorOrBasicNames actor) {
         return new ActorCreationResponseDTO(actor.getPrimaryName(),actor.getBirthYear(),actor.getDeathYear(), actor.getKnownForTitles(), actor.getIdnCost());
     }
+
+    public static ActorCreationResponseDTO convertPrincipalToDTO(PrincipalsTitle principalsTitle) {
+        return new ActorCreationResponseDTO(principalsTitle.getNConst(),principalsTitle.getOrdering(),principalsTitle.getJob(),principalsTitle.getIdTConst());
+    }
+
+    public static ActorCreationResponseDTO convertRatingToDTO(RattingTitle rattingTitle) {
+        return new ActorCreationResponseDTO(rattingTitle.getIdTConst(),rattingTitle.getNumVotes(),rattingTitle.getIdTConst(),rattingTitle.getIdTConst());
+    }
+
 }
