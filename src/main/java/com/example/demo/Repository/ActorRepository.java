9a0ef6bdbb5cdf65b792cf275942d08fbd393f43
mohamad <mhmm69923@gmail.com>
2023-01-04 23:24:02 +0330

    changes some code in class

--- src/main/java/com/example/demo/Repository/ActorRepository.java
+++ src/main/java/com/example/demo/Repository/ActorRepository.java
@@ -9,16 +9,22 @@
 package com.example.demo.Repository;
 
 import com.example.demo.moodel.ActorOrBasicNames;
+import org.attoparser.dom.INestableNode;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.Optional;
 
-public interface ActorRepository extends JpaRepository <ActorOrBasicNames, Long>{
+public interface ActorRepository extends JpaRepository <ActorOrBasicNames, Integer>{
 
-    Optional<ActorOrBasicNames> findByIdnCost( int intiger );
-    Optional<ActorOrBasicNames> findByPrimaryName( String st );
-    Optional<ActorOrBasicNames> findByKnownForTitles( int intigger);
-    Optional<ActorOrBasicNames> findBirthYear(int intigger);
-    Optional<ActorOrBasicNames> findByDeathYear( int intigger);
+    //Optional<ActorOrBasicNames> findByIdnCost( int intiger );
+    //Optional<ActorOrBasicNames> findByPrimaryName( String st );
+    List<ActorRepository> findByPrimaryName(String st);
+   // Optional<ActorOrBasicNames> findByKnownForTitles( int intigger);
+    List<ActorRepository> findByKnownForTitles(int i);
+   // Optional<ActorOrBasicNames> findBirthYear(int intigger);
+    List<ActorRepository>findByBirthYear(int i);
+   // Optional<ActorOrBasicNames> findByDeathYear( int intigger);
+    List<ActorRepository>findByDeathYear(int i);
 
 }
