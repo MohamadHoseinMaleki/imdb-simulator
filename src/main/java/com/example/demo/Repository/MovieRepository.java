6c42d36b994cf648fcaf0b8c23c3bef130cc1eaf
mohamad <mhmm69923@gmail.com>
2023-01-04 23:24:09 +0330

    changes some code in class

--- src/main/java/com/example/demo/Repository/MovieRepository.java
+++ src/main/java/com/example/demo/Repository/MovieRepository.java
@@ -12,16 +12,20 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.List;
 import java.util.Optional;
 
-public interface MovieRepository extends JpaRepository<MoviesOrBasics, Long> {
+public interface MovieRepository extends JpaRepository<MoviesOrBasics, Integer> {
 
-    Optional<MoviesOrBasics> findIsAdult(int i);
+   // Optional<MoviesOrBasics> findByIsAdult(Boolean bool);
+    List<MoviesOrBasics> findByIsAdult(Boolean bool);
 
-    Optional<MoviesOrBasics> findById(Long aLong);
+    //Optional<MoviesOrBasics> findById(Long aLong);
 
-    Optional<MoviesOrBasics> findstartYear(int startYear);
+    //Optional<MoviesOrBasics> findstartYear(int startYear);
+    List<MoviesOrBasics> findByStartYear (int statYear);
 
-    Optional<MoviesOrBasics> findendYear(int endYear);
-    Optional<MoviesOrBasics> findByMovieNum(String movieNum);
+//    Optional<MoviesOrBasics> findendYear(int endYear);
+    List<MoviesOrBasics>findByEndYear(int endYear);
+    //Optional<MoviesOrBasics> findByMovieNum(String movieNum);
 
-    List<MoviesOrBasics> findAll(String movie);
+  //  List<MoviesOrBasics> findAll(String movie);
+
 }
