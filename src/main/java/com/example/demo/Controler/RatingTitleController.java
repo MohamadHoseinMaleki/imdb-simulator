8303705d0b5a56d4d1052ffd8876f38023c77701
mohamad <mhmm69923@gmail.com>
2023-01-08 17:07:57 +0330

    changes some code in class

--- src/main/java/com/example/demo/Controler/RatingTitleController.java
+++ src/main/java/com/example/demo/Controler/RatingTitleController.java
@@ -13,23 +13,25 @@ import com.example.demo.moodel.RatingTitle;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import java.util.Optional;
+
 public class RatingTitleController {
 
     RatingTitleRepository ratingRepo;
 
 
     @GetMapping("getIdTConst")
-    public RatingTitle getIdTConst(@RequestParam String st){
-        return ratingRepo.findByIdTConst(st).orElse(null);
+    public Optional<RatingTitle> findById (@RequestParam Integer integer){
+        return ratingRepo.findById(integer);
     }
 
     @GetMapping("getAverageRating")
-    public RatingTitle getAverageRating(@RequestParam Double doubleNum){
-        return ratingRepo.findByAverageRating(doubleNum).orElse(null);
+    public RatingTitle getAverageRating(@RequestParam Integer doubleNum){
+        return (RatingTitle) ratingRepo.findByAverageRating(doubleNum);
     }
 
     @GetMapping("getNumVotes")
     public RatingTitle getNumVotes(@RequestParam Integer intNum){
-        return ratingRepo.findByNumVotes(intNum).orElse(null);
+        return (RatingTitle) ratingRepo.findByNumVotes(intNum);
     }
 }
