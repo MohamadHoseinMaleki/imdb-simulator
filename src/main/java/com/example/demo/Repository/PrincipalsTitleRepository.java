25585d770b177b494fccd313cd974c6815f871f8
mohamad <mhmm69923@gmail.com>
2023-01-08 17:07:51 +0330

    changes some code in class

--- src/main/java/com/example/demo/Repository/PrincipalsTitleRepository.java
+++ src/main/java/com/example/demo/Repository/PrincipalsTitleRepository.java
@@ -8,17 +8,26 @@
 
 package com.example.demo.Repository;
 
+import com.example.demo.moodel.PrincipalsTitle;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.Optional;
 
-public interface PrincipalsTitleRepository extends JpaRepository <PrincipalsTitleRepository, Integer> {
+public interface PrincipalsTitleRepository extends JpaRepository <PrincipalsTitle, Integer> {
 
-    Optional<PrincipalsTitleRepository> findByIdTConst(String st);
-    Optional<PrincipalsTitleRepository> findByCategory(String st);
-    Optional<PrincipalsTitleRepository> findByOrdering(String st);
-    Optional<PrincipalsTitleRepository> findBynConst(String st);
-    Optional<PrincipalsTitleRepository> findByJob(String st);
-    Optional<PrincipalsTitleRepository> findByCharacters(String st);
+   // Optional<PrincipalsTitleRepository> findByIdTConst(String st);
+
+    //Optional<PrincipalsTitleRepository> findByCategory(String st);
+    List<PrincipalsTitleRepository> findByCategory(String category);
+   // Optional<PrincipalsTitleRepository> findByOrdering(String st);
+    List<PrincipalsTitleRepository>findByOrdering(Integer integer);
+   // Optional<PrincipalsTitleRepository> findBynConst(String st);
+    List<PrincipalsTitleRepository>findByNConst(String NConst);
+    //Optional<PrincipalsTitleRepository> findByJob(String st);
+    List<PrincipalsTitleRepository>findByJob(String job);
+
+   // Optional<PrincipalsTitleRepository> findByCharacters(String st);
+    List<PrincipalsTitleRepository> findByCharacters(String characters);
 
 }
